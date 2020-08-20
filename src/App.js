40f4204edc847cb6ff2c0c27685abd59833ef0e2
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-08-20 14:41:21 +0200

    layout component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component }from 'react';
+
+import Layout from "../src/components/Layout/Layout"
 
 
 
-function App() {
-  return (
-    <div >
-     hello world!
-    </div>
-  );
+class App  extends Component {
+  render() {
+    return (
+      <div>
+      <Layout>
+      <p>buuu</p>
+      </Layout>
+     </div>
+    )
+
+   
+  }
+   
+
 }
 
 export default App;
